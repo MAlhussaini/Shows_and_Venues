ec048023134033b8bed838127bfe26ce2fb2f29a
vagrant <vagrant@vagrant.vm>
2021-09-24 18:17:19 +0300

    Complete Artists view functionality

--- app.py
+++ app.py
@@ -254,7 +254,6 @@ def show_venue(venue_id):
 #  Create Venue
 #  ----------------------------------------------------------------
 
-# TODO
 @app.route('/venues/create', methods=['GET'])
 def create_venue_form():
   form = VenueForm()
@@ -338,21 +337,11 @@ def delete_venue(venue_id):
 
 #  Artists
 #  ----------------------------------------------------------------
-# TODO
+# *Completed*
 @app.route('/artists')
 def artists():
-  # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
-  return render_template('pages/artists.html', artists=data)
+    data = Artists.query.all()
+    return render_template('pages/artists.html', artists=data)
 
 # *Completed*
 @app.route('/artists/search', methods=['POST'])
